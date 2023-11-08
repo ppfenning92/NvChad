930315e32eaf445da6556fcb3fb618094b1959de
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-11-08 15:37:14 +0100

    mappings....

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,3 +1,26 @@
 local M = {}
+M.general = {
+  i = {
+    ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
+  }    
+}
 
+
+M.comment = {
+
+   i = {
+    ["<C-#>"] = {
+      function()
+        require("Comment.api").toggle.linewise.current()
+      end,
+      "Toggle comment",
+    },
+  },
+
+  --   ["<C-#>"] = {
+  --     "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+  --     "Toggle comment",
+  --   },
+  -- },
+}
 return M
