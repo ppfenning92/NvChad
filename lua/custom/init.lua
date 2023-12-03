80ece88576e8a891dac95246b68cae5e682511b7
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2023-12-03 21:53:06 +0100

    maslöd

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,6 @@
 vim.wo.relativenumber = true
 vim.api.nvim_set_option_value("colorcolumn", "80", {})
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   update_in_insert = true,
 })
 -- vim.diagnostic.config {
