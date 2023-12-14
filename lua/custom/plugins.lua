3e3e54477b21e1bdfc1a047aa0da1eb435532be0
Patrick Pfenning 2 <patrick.pfenning.92@gmail.com>
2023-12-14 21:25:32 +0100

    add md preview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -130,6 +130,15 @@ local plugins = {
     "mg979/vim-visual-multi",
     event = "VeryLazy",
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && bun install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
 
 return plugins
