edd0a08884fe9588c838ee75b425f5dbe3ae5e82
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2024-02-16 21:01:26 +0100

    fix wsl detection

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,19 +6,25 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 -- vim.diagnostic.config {
 --   update_in_insert = true
 -- }
-vim.g.clipboard = {
-  name = "WslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 1,
-}
 
+_G.OS = os.execute "uname -a" or "Windows"
+_G.IS_LINUX = OS == "Linux"
+_G.IS_WSL = IS_LINUX and OS:lower():find "microsoft" and true or false
+
+if IS_WSL then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 1,
+  }
+end
 vim.keymap.set("i", "<F13>", 'copilot#Accept("<CR>")', {
   expr = true,
   replace_keycodes = false,
